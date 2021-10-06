b1d2e56d2c37c07219e12ba36e36a4e74ab4922c
Sabified <github@gogutten.no>
2021-10-06 16:59:11 +0200

    Modified main.py to be more flexible

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # Tokens
+
+TOKENS = {
+    "0": "ZERO",
+    "1": "ONE"
+}
+
 T_UNKNOWN = "UNKNOWN"
-T_ZERO = "ZERO"
-T_ONE = "ONE"
 
 tokens = []
 
@@ -9,10 +13,8 @@ def apply_tokens(string):
     current_character = 0
 
     for char in string:
-        if char == "0":
-            tokens.append(T_ZERO)
-        elif char == "1":
-            tokens.append(T_ONE)
+        if char in TOKENS:
+            tokens.append(TOKENS[char])
         else:
             print("Unknown Character: \'" + char + "\' Character " + str(current_character + 1))
             tokens.append(T_UNKNOWN)
